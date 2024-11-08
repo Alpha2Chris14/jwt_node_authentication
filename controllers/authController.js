6e551039efba4c5d45bfd9691b3172c9096c5b9b
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-11-08 21:04:48 +0100

    added User Model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,20 @@
+const User = require("../models/User");
+
 const signup_get = (req, res) => {
   res.render("signup");
 };
 const login_get = (req, res) => {
   res.render("login");
 };
-const signup_post = (req, res) => {
-  res.send("new signup");
+const signup_post = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("error,user not created");
+  }
 };
 const login_post = (req, res) => {
   const { email, password } = req.body;
