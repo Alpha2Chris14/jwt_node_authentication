cdba8299046a770a058f5d62b1ed433a6b28b484
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-11-13 23:53:31 +0100

    added hashed password using bycypt and fixed issues

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bycrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -13,6 +14,20 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter an password"],
     minlength: [6, "Mininum password length is 6 characters"],
   },
+});
+
+//fires function after document has been saved
+userSchema.post("save", (doc, next) => {
+  console.log("New user was saved", doc);
+  next();
+});
+
+//funtion run before saving data to db
+userSchema.pre("save", async function (next) {
+  const salt = await bycrypt.genSalt();
+  this.password = await bycrypt.hash(this.password, salt);
+  console.log("user about to be saved", this);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
